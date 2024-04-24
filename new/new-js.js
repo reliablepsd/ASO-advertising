503da91d3d634f32241506ea87f01d8ba0badacc
sharnirio <sharnirio@gmail.com>
2024-04-24 11:08:33 +0300

    Refactoring JS

--- new/new-js.js
+++ new/new-js.js
@@ -17,16 +17,37 @@ function isElementExist(_el, _cb) {
 }
 
 function btnMore() {
-  jQuery(".flavor-btns").closest(".flavor-row-new").addClass("btn-on-mob")
+
   jQuery(".flavor-row-new").each(function (index) {
     let i = 1,
-        startElement = 32,
-        amountElement = 18;
-    jQuery(this).on("click", ".flavor-btn-more", function () {
-      jQuery(this).closest(".flavor-row-new").addClass("flavor-" + index).find(jQuery("style")).remove()
-      jQuery(this).closest(".flavor-row-new").append(
-          "<style>@media (max-width: 1250px) {.btn-on-mob.flavor-" + index +" .flavor-new-item:nth-of-type(-n+" + (startElement + i * amountElement) +") { display: block; }}</style>")
-      i += 1
-    })
+      startElement = 32,
+      amountFlavor = jQuery(this).find(".flavor-new-item").length,
+      amountElement = 18;
+
+    if (amountFlavor > (startElement + i * amountElement)) {
+      jQuery(this).addClass("btn-on-mob").append('<div class="flavor-btns"><a href="#!" class="flavor-btn-more">LOAD MORE FLAVORS</a></div>')
+
+      jQuery(this).on("click", ".flavor-btn-more", function () {
+        if ((startElement + i * amountElement) < amountFlavor) {
+          jQuery(this)
+            .closest(".flavor-row-new")
+            .addClass("flavor-" + index)
+            .find(jQuery("style"))
+            .remove()
+          jQuery(this)
+            .closest(".flavor-row-new")
+            .append(
+              "<style>@media (max-width: 1250px) {.btn-on-mob.flavor-" +
+                index +
+                " .flavor-new-item:nth-of-type(-n+" +
+                (startElement + i * amountElement) +
+                ") { display: block; }}</style>"
+            )
+          i += 1
+        } else {
+          jQuery(this).closest(".flavor-row-new").addClass("flavor-all-visible")
+        }
+      })
+    }
   })
 }
