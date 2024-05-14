10629773834981b4e8b46c66916b82061a185d5b
sharnirio <sharnirio@gmail.com>
2024-05-14 18:54:59 +0300

    create hide button

--- new/new-js.js
+++ new/new-js.js
@@ -21,7 +21,7 @@ function btnMore() {
   jQuery(".flavor-new-item._headline").closest(".flavor-row-new").addClass("_have-headline");
 
   jQuery(".flavor-row-new").each(function (index, element) {
-    jQuery(".flavor-row-new")
+
     const startElement = 32;
     const amountElement = 18;
     const $this = jQuery(this);
@@ -30,19 +30,29 @@ function btnMore() {
     let i = 1;
 
     if (amountFlavor > startElement + i * amountElement) {
-      $this.addClass("btn-on-mob").append('<div class="flavor-btns"><a href="#!" class="flavor-btn-more">LOAD MORE FLAVORS</a></div>');
+      $this.addClass("btn-on-mob flavor-all-hide").append('<div class="flavor-btns"><a href="#!" class="flavor-btn-hide">HIDE FLAVORS</a><a href="#!" class="flavor-btn-more">LOAD MORE FLAVORS</a></div>');
 
-      $this.on("click", ".flavor-btn-more", function () {
-        const visibleElements = startElement + i * amountElement;
-        const styleString = `<style>@media (max-width: 1250px) {.btn-on-mob.flavor-${index} .flavor-new-item:nth-of-type(-n+${visibleElements}) { display: block; }}</style>`;
-
-        if (visibleElements < amountFlavor) {
-          $this.addClass(`flavor-${index}`).find("style").remove().end().append(styleString);
+      $this.on("click", ".flavor-btns > a", function () {
+        let visibleElements = startElement;
+        if (jQuery(this).hasClass("flavor-btn-more")){
+          visibleElements = startElement + i * amountElement;
           i++;
-        } else {
-          $this.addClass(`flavor-${index}`).find("style").remove().end().append(styleString);
-          $this.addClass("flavor-all-visible");
+          $this.removeClass("flavor-all-hide");
+          if (visibleElements >= amountFlavor) {
+            $this.addClass("flavor-all-visible").removeClass("flavor-all-hide");
+          }
+        } else if (jQuery(this).hasClass("flavor-btn-hide")){
+          i--;
+          visibleElements = startElement + (i - 1) * amountElement;
+          $this.removeClass("flavor-all-visible");
+          if (visibleElements <= startElement) {
+            $this.addClass("flavor-all-hide");
+          }
         }
+        let styleString = `<style>@media (max-width: 1250px) {.btn-on-mob.flavor-${index} .flavor-new-item:nth-of-type(-n+${visibleElements}) { display: block; }}</style>`;
+
+        $this.addClass(`flavor-${index}`).find("style").remove().end().append(styleString);
+
       });
     }
   });
